63725ff1c9280c67be4fea6d97e44d1e11332949
seif <s.toema04@gmail.com>
2023-08-15 10:17:05 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
- *                  the alphabet a - z
+ *		    the alphabet a - z
 */
 
 void print_alphabet(void)
@@ -10,6 +10,6 @@ void print_alphabet(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-                _putchar(ch);
-        _putchar('\n');
+		 _putchar(ch);
+	_putchar('\n');
 }
