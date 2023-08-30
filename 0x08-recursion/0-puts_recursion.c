846a0e86153084383eae3aa5abc23bf6b36b6967
seif <s.toema04@gmail.com>
2023-08-30 14:59:08 -0700

    m

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
+ * -puts_recursion - prints a string, followed by a new line
  * @s: pointer to the string
  * Return: void
 */
 
-void _puts_recursion(char *s)
+void -puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
