a91f9a443203c500e7667a6cc3b7f92a04e678af
seif <s.toema04@gmail.com>
2023-09-11 10:33:07 -0700

    z

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,11 +2,12 @@
 
 /**
  * init_dog - initializes a dog
+ * @d: the dog to init
  * @name: the dog's name
  * @age: dog's age
  * @owner: owner's name
  *
- * Return: void. 
+ * Return: void.
 */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
