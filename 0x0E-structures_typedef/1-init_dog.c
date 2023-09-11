9aa4c123d87a83bf7bd6b28e56320105960f3984
seif <s.toema04@gmail.com>
2023-09-11 09:57:06 -0700

    z

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,19 @@
-#ifndef _DOG_H
-#define _DOG_H
+#include "dog.h"
 
 /**
- * struct dog - a dog struct
- * @name: dog's name
+ * init_dog - initializes a dog
+ * @name: the dog's name
  * @age: dog's age
  * @owner: owner's name
  *
- * description: just a long dog in a big kitty world
+ * Return: void. 
 */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-#endif
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
