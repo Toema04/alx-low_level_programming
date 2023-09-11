6bf1f9681ca0685758d5d9f40931188416c6236d
seif <s.toema04@gmail.com>
2023-09-11 08:55:27 -0700

    z

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -7,26 +7,6 @@
 */
 int main(void)
 {
-        printf("%s\n", __)
-#include <stdio.h>
-
-/**
- * main - print name of of this source file
- *
- * Return: Always 0.
-*/
-int main(void)
-{
-        printf("%s\n", __)
-#include <stdio.h>
-
-/**
- * main - print name of of this source file
- *
- * Return: Always 0.
-*/
-int main(void)
-{
 	printf("%s\n", __FILE__);
 	return (0);
 }
